8b46e06ad5e74e40ea6efc4a05df18bf2a64f4d6
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-12-21 00:16:44 +0600

    how to pass a catch block to parent

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// nested try catch 
+// https://stackoverflow.com/questions/10727025/javascript-pass
+let myVariable = 'fyf';
+
+try {
+  if(3>1){
+    console.log('three > one');
+    try {
+      if(5>6){
+        console.log('five > six');
+      }else{
+        throw new Error('i am chaild error, throw to next catch')
+      }
+    }catch (error) {
+      throw error
+      // console.log(error);
+    }
+  }else{
+    throw new Error('i am parent error')
+  }
+} catch (error) {
+  console.log('i am parrent ')
+  console.error(error)
+}
